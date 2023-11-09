76c53325c7c5106b895675c13e356c99baaec897
yatender <124253233+uehs1@users.noreply.github.com>
2023-11-09 15:19:26 +0530

    Add files via upload

---
+++ src/app/weather.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor(private http: HttpClient) { }
+
+  getweather(city: string, units: string) {
+    return this.http.get('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&{your_Open_API_Key}&units=' + units);
+  }
+}
